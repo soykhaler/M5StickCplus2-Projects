1ce4bd8c290ef6c544077a1ff7950033c7f864ec
SoyKhaler <74861088+soykhaler@users.noreply.github.com>
2024-11-25 01:10:22 +0100

    Update SplashScreen.cpp

--- SplashScreen.cpp
+++ SplashScreen.cpp
@@ -2,11 +2,10 @@
 
 void showSplashScreen() {
     StickCP2.Display.clear(TFT_BLACK);
-    StickCP2.Display.setTextColor(TFT_WHITE);
+    StickCP2.Display.setTextColor(TFT_PURPLE);
     StickCP2.Display.setCursor(0, 0); 
     StickCP2.Display.setFont(&fonts::Font2); 
     StickCP2.Display.setTextSize(2); 
-    StickCP2.Display.setTextColor(TFT_PURPLE);
     StickCP2.Display.println("SoyKhaler");
     StickCP2.Display.println("Homebrew");
     StickCP2.Display.println("Firmware");
